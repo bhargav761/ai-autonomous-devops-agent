e7bcef9eb477dfc7dcc8968868af62b735d33247
Bhargava R <bhargav761@gmail.com>
2026-01-28 14:44:48 +0530

    Break health check for rollback test

--- app/backend/app.py
+++ app/backend/app.py
@@ -10,7 +10,7 @@ def home():
 
 @app.route("/health")
 def health():
-    return jsonify({"status": "healthy"}), 500
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
